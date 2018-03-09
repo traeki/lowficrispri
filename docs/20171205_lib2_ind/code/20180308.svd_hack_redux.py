495645c58366be1cbdaad957965357ab3d8b401e
John Hawkins <github@disquieting.com>
2018-03-08 19:04:04 -0800

    After talk with Sy, about to try ruling out failure modes.
    Former-commit-id: b4ba6beec7d50283cbd234024948aeced2a5d512

--- docs/20171205_lib2_ind/code/20180308.svd_hack_redux.py
+++ docs/20171205_lib2_ind/code/20180308.svd_hack_redux.py
@@ -89,11 +89,12 @@ def flip_signs(A, B):
 widediffdata = widen_groups(diffdata)
 cleaned = impute_over_nans(widediffdata)
 scaler = preprocessing.StandardScaler(with_mean=True, with_std=False)
-subset = cleaned[all_no_drug_spans()]
+# subset = cleaned[all_no_drug_spans()]
+subset = cleaned
 # transposing to scale along the strains, instead of along the measurments
-X = scaler.fit_transform(subset.T)
+X = scaler.fit_transform(subset.T).T
 
-# SVDecompose X directly into USVt
+# SVDecompose X.T directly into USVt
 U, s, Vt = np.linalg.svd(X, full_matrices=False)
 V = Vt.T
 S = np.diag(s)
@@ -105,10 +106,58 @@ assert np.allclose(V.dot(S.T.dot(U.T)), X.T)
 # U S V.T == X
 assert np.allclose(U.dot(S.dot(V.T)), X)
 
-PC_names = ['PC{0}'.format(i+1) for i in range(len(Vt))]
-rated_guides = pd.DataFrame(V, columns=PC_names, index=cleaned.index)
-cutoff = 4*(rated_guides.std())
-hits = rated_guides > cutoff
+n = X.shape[0]
+L = np.diag((s**2)/(n-1))
+C = V.dot(L.dot(Vt))
+
+#### <Mask Code>
+sample_ids = subset.columns.get_level_values(0)
+print sample_ids
+dim = len(sample_ids)
+tubes = [x[0]+x[2:] for x in sample_ids]
+spans = [x[1] for x in sample_ids]
+def dose(sample):
+  if sample[0] == 'a':
+    return 0
+  if sample[2:] == 'd1':
+    return 0
+  if sample[2:] == 'd2':
+    return 7.5
+  if sample[2:] == 'd3':
+    return 30
+  logging.fatal('dose(sample) choked on {sample}'.format(**vars()))
+  sys.exit(2)
+doses = [dose(x) for x in sample_ids]
+def agreement(tags):
+  return lambda i, j: tags[i] == tags[j]
+sametube = np.fromfunction(np.vectorize(agreement(tubes)),
+                           (dim, dim), dtype=int)
+samespan = np.fromfunction(np.vectorize(agreement(spans)),
+                           (dim, dim), dtype=int)
+samedose = np.fromfunction(np.vectorize(agreement(doses)),
+                           (dim, dim), dtype=int)
+#### </Mask Code>
+
+mask = samedose & samespan
+print mask
+# mask = np.ones_like(C)
+
+C_m = (C * mask)
+l_m, V_m = np.linalg.eig(C_m)
+idx = l_m.argsort()[::-1]
+l_m = l_m[idx]
+V_m = V_m[:,idx]
+C_m_check = V_m.dot(np.diag(l_m).dot(V_m.T))
+assert np.allclose(C_m, C_m_check)
+s_m = np.sqrt(np.abs(l_m)*(n-1))
+S_m = np.diag(s_m)
+S_m_inv = np.linalg.inv(S_m)
+U_m = X.dot(V_m.dot(S_m_inv))
+
+PC_names = ['PC{0}'.format(i+1) for i in range(len(U_m.T))]
+rated_guides = pd.DataFrame(U_m, columns=PC_names, index=cleaned.index)
+cutoff = 2*(rated_guides.std())
+hits = rated_guides.abs() > cutoff
 masked = cleaned.copy()
 masked['hits'] = hits.PC3
 
